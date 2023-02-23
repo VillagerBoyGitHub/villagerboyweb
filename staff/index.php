af1f676671e50e394bbad6d47ce44534b9abbd31
Villager Boy GitHub <103837860+VillagerBoyGitHub@users.noreply.github.com>
2023-02-23 17:43:30 +0100

    Update index.php

--- staff/index.php
+++ staff/index.php
@@ -1,1 +1,32 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Villager Boy Web</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
+    <meta name="description" content="Villager Boy is a Youtuber which owns a fun Discord and Minecraft server.">
+    <meta name="keywords" content="Villager Boy Host, Villager Boy Web, Minecraft servers, top cracked minecraft servers, gaming, community, discord servers, child friendly, vboy.it, play.vboy.it">
+    <meta name="author" content="John Doe">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="assets/js/jquery.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Staff | Villager Boy Web</title>
+<div>
+    <a href="https://vboy.it">Home</a>
+    <a class="menu" href="https://vboy.it/staff">Staff</a>
+    <a class="menu" href="https://vboy.it/villager-client">Villager Client</a>
+</div>
+    <center><h1 class="title">Home</h1></center>
+</head>
 
+<body>
+
+  Please select an option: 
+  <a class="vboy-buttons">Moderator Application</a>
+  <a class="vboy-buttons">Developer Application</a>
+  
+</body>
